9d49ac8f696385159be5bc8870a8b17c6121146b
Valeria Rodriguez <val.lilou.rod@gmail.com>
2021-05-26 15:41:02 -0600

    inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,44 @@
 // TODO: Include packages needed for this application
-
 // TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message:'What is the title of your project?',
+        name:'title',
+    },
+    {
+        type: 'input',
+        message:'Describe your project-',
+        name:'description',
+    },  {
+        type: 'input',
+        message:'Describe your installation process-',
+        name:'installation',
+    },  {
+        type: 'input',
+        message:'Provide usage information-',
+        name:'usage',
+    },  {
+        type: 'input',
+        message:'What are the contribution guidelines?',
+        name:'contribution',
+    },  {
+        type: 'input',
+        message:'What are your testing instructions?',
+        name:'instructions',
+    },
+
+])
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
