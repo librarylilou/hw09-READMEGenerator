bc049d3f061a3b2781baf8814967f47f30a95e93
Valeria Rodriguez <val.lilou.rod@gmail.com>
2021-05-26 18:06:13 -0600

    console.logData

--- Develop/index.js
+++ Develop/index.js
@@ -1,37 +1,52 @@
 // TODO: Include packages needed for this application
 // TODO: Create an array of questions for user input
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 inquirer.prompt([
     {
         type: 'input',
-        message:'What is the title of your project?',
-        name:'title',
+        message: 'What is your GitHub username?',
+        name: 'username',
     },
     {
         type: 'input',
-        message:'Describe your project-',
-        name:'description',
-    },  {
+        message: 'What is your email?',
+        name: 'email',
+    },
+    {
         type: 'input',
-        message:'Describe your installation process-',
-        name:'installation',
-    },  {
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
         type: 'input',
-        message:'Provide usage information-',
-        name:'usage',
-    },  {
+        message: 'Describe your project-',
+        name: 'description',
+    }, {
         type: 'input',
-        message:'What are the contribution guidelines?',
-        name:'contribution',
-    },  {
+        message: 'Describe your installation process-',
+        name: 'installation',
+    }, {
         type: 'input',
-        message:'What are your testing instructions?',
-        name:'instructions',
+        message: 'Provide usage information-',
+        name: 'usage',
+    }, {
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name: 'contribution',
+    }, {
+        type: 'input',
+        message: 'What are your testing instructions?',
+        name: 'instructions',
     },
 
 ])
-
+    .then((response) =>
+        response.data === response.message
+            ? console.log('PARTY!')
+            : console.log('UHM?')
+    );
 
 
 // TODO: Create a function to write README file
